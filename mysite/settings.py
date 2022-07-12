b8599a25c07dfa056a6f49f547f757559cd74dcc
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2022-07-12 10:38:27 +0400

    debug_off

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w@xb*4#7!0et3d!5(7(rg7@wu0s3x9^-6srkzueslr9kbv5-3='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['chalexrol.pythonanywhere.com', '127.0.0.1']
 
