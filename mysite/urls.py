d75d5fbadac21388e2caf3285e289ec6022feb22
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2022-07-12 12:47:05 +0400

    new_app2

--- mysite/urls.py
+++ mysite/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
-    path('account/', include('account.urls')),
+    path('account/', include('account.urls', namespace='account')),
 ]
