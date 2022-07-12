7360f8b6815b8b7485fc452a379867c24c69c9b1
Alexandr Chukhvantsev <mob.pochta@gmail.com>
2022-07-12 10:34:04 +0400

    add_feed_rss

---
+++ blog/sitemaps.py
@@ -0,0 +1,15 @@
+from django.contrib.sitemaps import Sitemap
+from .models import Post
+
+
+class PostSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.9
+    protocol = 'https'
+
+    def items(self):
+        return Post.published.all()
+
+    @staticmethod
+    def lastmod(obj):
+        return obj.updated
